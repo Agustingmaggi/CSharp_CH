f07d71a13dda93573b10331c953b5ee4c7ba651e
agustin garcia maggi <agustingmaggi@gmail.com>
2024-01-16 08:28:54 +0100

    primer commit del tp final

---
+++ Producto.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Producto
+    {
+{
+    private int _id;
+        private string _descripcion;
+        private decimal _costo;
+        private decimal _precioVenta;
+        private int _stock;
+        private int _idUsuario;
+
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        public string Descripcion
+        {
+            get { return _descripcion; }
+            set { _descripcion = value; }
+        }
+
+        public decimal Costo
+        {
+            get { return _costo; }
+            set { _costo = value; }
+        }
+
+        public decimal PrecioVenta
+        {
+            get { return _precioVenta; }
+            set { _precioVenta = value; }
+        }
+
+        public int Stock
+        {
+            get { return _stock; }
+            set { _stock = value; }
+        }
+
+        public int IdUsuario
+        {
+            get { return _idUsuario; }
+            set { _idUsuario = value; }
+        }
+    }
+}
+}
---
+++ Usuario.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+public class Usuario
+{
+    public int Id { get; set; }
+    public string Nombre { get; set; }
+    public string Apellido { get; set;}
+    public string nombreUsuario { get; set; }
+    public string Contrasenia { get; set; }
+    public string email { get; set; }
+
+
+}
